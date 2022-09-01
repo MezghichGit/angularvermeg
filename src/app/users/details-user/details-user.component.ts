bcc152d458e2aba2f0f7051fc022656360933645
moahmed amine mezghich <git@mezghich.com>
2022-09-01 16:34:09 +0100

    user-details

--- src/app/users/details-user/details-user.component.ts
+++ src/app/users/details-user/details-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service'
 @Component({
   selector: 'app-details-user',
   templateUrl: './details-user.component.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsUserComponent implements OnInit {
 
-  constructor() { }
+  userId: any;
+  user:any;
+  constructor(private userService: UserService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        this.userId = params.get('id');
+      }
+    );
+
+    this.userService.getUserDetails(this.userId).subscribe(
+      user =>{
+        this.user=user;
+        console.log(user);
+      }
+    );
   }
 
 }
